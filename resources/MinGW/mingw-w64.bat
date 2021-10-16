780d0634d15ed0f3fac5d38960a46faa5aff9921
Mighty Pancake <MightyPancake@users.noreply.github.com>
2021-10-16 19:32:48 +0200

    initial release

---
+++ resources/MinGW/mingw-w64.bat
@@ -0,0 +1,6 @@
+echo off
+set PATH=E:\MinGW\mingw64\bin;%PATH%
+rem echo %PATH%
+rem cd "E:\MinGW\mingw64\bin"
+cd "C:\"
+"C:\WINDOWS\system32\cmd.exe"
---
+++ resources/prep.bat
@@ -0,0 +1,7 @@
+PATH %PATH%;C:\Users\fikr1\Documents\GitHub\MightyPancake\pancake-launcher\resources\MinGW\mingw64\bin
+PATH %PATH%;C:\Users\fikr1\Documents\GitHub\MightyPancake\pancake-launcher\resources\MinGW\mingw64\x86_64-w64-mingw32\bin
+cd C:\Users\fikr1\Desktop\Pancake\Test
+TITLE Pancake Debug Window
+CLS
+go run main.go
+EXIT
